ac3c86b379c0235b39f4df9eb679535273f8b2fb
Damian Szatkowski <damianszatkowski387@gmail.com>
2020-07-24 15:03:50 +0200

    initial project commit

---
+++ src/test/java/pl/umk/mat/damianszat/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umk.mat.damianszat.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
